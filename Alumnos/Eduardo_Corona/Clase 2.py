16dc6ebe444f665e044d7ec7c3254404ac02053a
Eduardo_Corona <eduard083vc2@gmail.com>
2025-10-16 20:37:23 -0600

    Hola

---
+++ Alumnos/Eduardo_Corona/Clase 2.py
@@ -0,0 +1,64 @@
+numero = 5 #integer
+decimal = 3.5 #float 
+string = 'Hola' #string
+booleano = True #boolean. Valores False o True
+
+
+numero2 = "2"
+
+
+suma2 = numero + int(numero2)
+
+
+# Operaciones Aritmeticas
+
+suma = numero + numero2
+resta = numero - numero2
+multiplicacion = numero * numero2
+division = numero / numero2
+modulo = numero % numero2
+
+
+print(suma2)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+
+
+
+print("-----------------------------------------------------------------------------------------")
+
+
+
+nombre = "Emiliano"
+edad = 23
+activo = True
+
+print("Hola " + nombre)
+print("Tu edad es: " + str(edad))
+print("Estas activo: " + str(activo))
+
+print("-----------------------------")
+
+nombre = input("Cual es tu nombre?: ")
+edad = input("Cual es tu edad?: ")
+activo = input("Estas activo?: ")
+
+int(edad)
+str(activo)
+float(edad)
+bool(activo)
+
+
+print("Hola " + nombre)
+print("Tu edad es: " + edad)
+print("Estas activo: " + activo)
+
+print("-----------------------------")
+numero1 = int(input("Ingresa un numero: "))
+numero2 = int(input("Ingresa otro numero: "))
+print(numero1 + numero2) 
+
+print("-----------------------------")
+
